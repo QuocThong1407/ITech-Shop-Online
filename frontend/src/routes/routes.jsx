8c67591a0f0b16035c74125ff58b150b6c534233
Truong Quoc Thong <truongquocthong07.14@gmail.com>
2025-12-29 23:22:09 +0700

    feat/add admin layout

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -3,6 +3,7 @@ import Home from "../pages/customer/Home/Home.jsx";
 import AdminLayout from "../components/Layouts/AdminLayout/AdminLayout.jsx";
 import Dashboard from "../pages/admin/Dashboard/Dashboard.jsx";
 import CustomerLayout from "../components/Layouts/CustomerLayout/CustomerLayout.jsx";
+import SellerLayout from "../components/Layouts/SellerLayout/SellerLayout.jsx";
 
 export const routes = [
     {
@@ -18,6 +19,16 @@ export const routes = [
     {
         path: '/admin',
         element: <AdminLayout/>,
+        children: [
+            {
+                path: 'dashboard',
+                element: <Dashboard/>
+            },
+        ]
+    },
+    {
+        path: '/seller',
+        element: <SellerLayout/>,
         children: [
             {
                 path: 'dashboard',
